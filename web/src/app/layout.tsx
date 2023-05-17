190fc5e0f2306b222822939cc62c70c0b9d470b2
João Casagrande <pngbighouse@gmail.com>
2023-05-17 19:38:03 -0300

    readição das pastas web e mobile

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'Nlw Spacetime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCss e Typescript',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
